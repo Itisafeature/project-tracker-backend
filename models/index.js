ccd488b79bfe4e293bec7e1eeb8e8bac6bea8f26
Itisafeature <thebigdebugger@gmail.com>
2021-01-31 17:33:22 -0500

    working on postgres setup

---
+++ models/index.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(
+  `${process.env.DB}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWD}`,
+  {
+    dialect: 'postgres',
+  }
+);
+
+const models = {
+  User: sequelize.import('./user'),
+};
+
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
---
+++ models/user.js
@@ -0,0 +1,13 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+  });
+
+  return User;
+};
+
+export default user;
