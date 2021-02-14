3d9167fd67fa16d95f8836ab34f7ee070d3dc53d
Itisafeature <thebigdebugger@gmail.com>
2021-02-13 19:23:27 -0500

    added item model/table

---
+++ models/item.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Item.belongsTo(models.Board, {
+        foreignKey: 'boardId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+  Item.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      boardId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Item',
+    }
+  );
+  return Item;
+};
