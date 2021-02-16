0911bd74244ecc42399eb03706abe1d984a32107
Itisafeature <thebigdebugger@gmail.com>
2021-02-16 18:12:01 -0500

    lots done on crud board actions

--- controllers/boardsController.js
+++ controllers/boardsController.js
@@ -2,6 +2,32 @@ const Board = require('../models').Board;
 const Item = require('../models').Item;
 const User = require('../models').User;
 
+exports.getBoards = async (req, res, next) => {
+  try {
+    const boards = await req.user.getBoards({
+      attributes: { exclude: ['userId'] },
+    });
+    res.status(200).json({
+      status: 'success',
+      boards,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getBoard = async (req, res, next) => {
+  try {
+    const board = await req.user.getBoards({
+      where: { name: req.params.boardName },
+      include: [Item],
+    });
+    console.log(board);
+  } catch (err) {
+    next(err);
+  }
+};
+
 exports.createBoard = async (req, res, next) => {
   try {
     const board = await Board.create(
--- routes/board.js
+++ routes/board.js
@@ -4,6 +4,8 @@ const boardsController = require('../controllers/boardsController');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
+router.get('/', authController.protect, boardsController.getBoards);
+router.get('/:boardName', authController.protect, boardsController.getBoard);
 router.post('/new', authController.protect, boardsController.createBoard);
 
 module.exports = router;
